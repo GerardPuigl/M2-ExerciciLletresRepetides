29882bc9ed1168c5151693d9c6d61ffc798800c2
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-26 00:37:21 +0100

    Update Fase 4 Completada

--- src/LletresRepetidesAPP.java
+++ src/LletresRepetidesAPP.java
@@ -1,24 +1,15 @@
 import java.util.*;
-import javax.swing.JOptionPane;
 
 public class LletresRepetidesAPP {
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 
-		// Petición nombre mediante panel
+		// Nom amb String
 
-<<<<<<< HEAD
-		String name = JOptionPane.showInputDialog("Escriu el teu nom");
-		
-		String surname = JOptionPane.showInputDialog("Escriu el teu cognom");
-		
-		// Convertir el nombre en una Arraylist
-=======
 		String name = "Gerard";
 		String surname = "Puig";
 
 		// Convertir el nombre en una Arraylist 
->>>>>>> refs/heads/Fase_4
 
 		ArrayList<Character> arraylistname = new ArrayList();
 
@@ -26,45 +17,10 @@ public class LletresRepetidesAPP {
 			arraylistname.add(name.charAt(i));
 		}
 
-<<<<<<< HEAD
-		// Añadir lista apellido 
-		
-		ArrayList<Character> arraylistsurname = new ArrayList();
-=======
 		// Convertir el apellido en Arraylist
->>>>>>> refs/heads/Fase_4
 
-<<<<<<< HEAD
-		for (int i = 0; i < surname.length(); i++) {
-			arraylistsurname.add(surname.charAt(i));
-		}
-=======
 		ArrayList<Character> arraylistsurname = new ArrayList();
->>>>>>> refs/heads/Fase_4
 
-<<<<<<< HEAD
-		// Fusionar las dos listas
-	
-		ArrayList<Character> arraylistFullName = new ArrayList();
-		
-		for (char letra : arraylistname) {
-			arraylistFullName.add(letra);			
-		}
-		
-		arraylistFullName.add(' ');
-
-		for (char letra : arraylistsurname) {
-			arraylistFullName.add(letra);
-		}
-
-		for (char letra : arraylistFullName) {
-			System.out.print(letra);	
-		}
-		
-		System.out.println("");
-
-		// Imprime si son vocales o consonantes y detecta números o caracteres especiales
-=======
 		for (int i = 0; i < surname.length(); i++) {
 			arraylistsurname.add(surname.charAt(i));
 		}
@@ -92,7 +48,6 @@ public class LletresRepetidesAPP {
 		System.out.println("");
 
 		// Imprime si son vocales o consonantes i detecta números o caracters extranys
->>>>>>> refs/heads/Fase_4
 
 		for (int i = 0; i < arraylistFullName.size(); i++) {
 
@@ -100,58 +55,29 @@ public class LletresRepetidesAPP {
 
 			if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
 
-<<<<<<< HEAD
-				System.out.println(arraylistFullName.get(i) + " és una vocal");
-=======
 				System.out.println(arraylistFullName.get(i) + " VOCAL");
->>>>>>> refs/heads/Fase_4
 
 			} else if (Character.isDigit(letra)) {
 
-<<<<<<< HEAD
-				System.out.println(arraylistFullName.get(i) + " Error, els noms de persones no contenen números!");
-			
-			} else if (letra==' '){
-			
-				//no hacer nada
-				
-			} else if (Character.isAlphabetic(letra) == false) {
-
-				System.out.println(arraylistFullName.get(i) + " Error, els noms no contenen caracters especials!");
-=======
 				System.out.println(arraylistFullName.get(i) + " Els noms de persones no contenen números!");
 
 			} else if (letra == ' ') {
 
 				// no hacer nada
->>>>>>> refs/heads/Fase_4
 
 			} else {
 
-<<<<<<< HEAD
-				System.out.println(arraylistFullName.get(i) + " és una consonant");
-=======
 				System.out.println(arraylistFullName.get(i) + " CONSONANT");
->>>>>>> refs/heads/Fase_4
 
 			}
 		}
 
-<<<<<<< HEAD
-		//Mapear letras nombre (FASE 3 Ejercicio)
-		
-		HashMap<Character,Integer> contadorletras = new HashMap<Character,Integer>();
-		
-		for (char l : arraylistFullName) {
-			
-=======
 		// Mapear letras nombre completo
 
 		Map<Character, Integer> contadorletras = new HashMap<Character, Integer>();
 
 		for (char l : arraylistFullName) {
 
->>>>>>> refs/heads/Fase_4
 			char key = Character.toUpperCase(l);
 
 			if (Character.isAlphabetic(key)) {
@@ -169,33 +95,9 @@ public class LletresRepetidesAPP {
 
 		}
 
-<<<<<<< HEAD
-		//Mostrar en pantalla los datos
-		
-		//imprimir todo del tiron
-		
-		System.out.println(contadorletras.entrySet()); 
-		
-		//impresión por letra
-		
-		for (char key : contadorletras.keySet()) {
-			
-			if (contadorletras.get(key) == 1) {
-			
-				System.out.println("Té " + contadorletras.get(key) + " lletra " + key);
-			
-			} else {
-			
-				System.out.println("Té " + contadorletras.get(key) + " lletres " + key);
-			
-			}
-		}
-		
-=======
 		// Mostrar en pantalla los datos
 
 		System.out.println(contadorletras.entrySet());		
 
->>>>>>> refs/heads/Fase_4
 	}
 }
