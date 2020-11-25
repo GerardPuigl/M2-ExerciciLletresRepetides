28308d862366a3be6426940990e25871e7d3e6e1
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-26 00:18:43 +0100

    Update Fase 4 Completada

--- src/LletresRepetidesAPP.java
+++ src/LletresRepetidesAPP.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 public class LletresRepetidesAPP {
 
 	public static void main(String[] args) {
@@ -8,8 +7,9 @@ public class LletresRepetidesAPP {
 		// Nom amb String
 
 		String name = "Gerard";
+		String surname = "Puig";
 
-		// Convertir el nombre en una Arraylist
+		// Convertir el nombre en una Arraylist 
 
 		ArrayList<Character> arraylistname = new ArrayList();
 
@@ -17,45 +17,77 @@ public class LletresRepetidesAPP {
 			arraylistname.add(name.charAt(i));
 		}
 
-		// Imprime si son vocales o consonantes y detecta números
+		// Convertir el apellido en Arraylist
 
-		for (int i = 0; i < arraylistname.size(); i++) {
+		ArrayList<Character> arraylistsurname = new ArrayList();
 
-			char letra = Character.toUpperCase(arraylistname.get(i));
+		for (int i = 0; i < surname.length(); i++) {
+			arraylistsurname.add(surname.charAt(i));
+		}
+
+		// Fusionar las dos listas con un espacio en medio
+
+		ArrayList<Character> arraylistFullName = new ArrayList();
+
+		for (char letra : arraylistname) {
+			arraylistFullName.add(letra);
+		}
+
+		arraylistFullName.add(' ');
+
+		for (char letra : arraylistsurname) {
+			arraylistFullName.add(letra);
+		}
+
+		// Imprime el nombre completo con espacio
+		
+		for (char letra : arraylistFullName) {
+			System.out.print(letra);
+		}
+
+		System.out.println("");
+
+		// Imprime si son vocales o consonantes i detecta números o caracters extranys
+
+		for (int i = 0; i < arraylistFullName.size(); i++) {
+
+			char letra = Character.toUpperCase(arraylistFullName.get(i));
 
 			if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
 
-				System.out.println(arraylistname.get(i) + " VOCAL");
+				System.out.println(arraylistFullName.get(i) + " VOCAL");
 
 			} else if (Character.isDigit(letra)) {
 
-				System.out.println(arraylistname.get(i) + " Els noms de persones no contenen números!");
+				System.out.println(arraylistFullName.get(i) + " Els noms de persones no contenen números!");
+
+			} else if (letra == ' ') {
+
+				// no hacer nada
 
 			} else {
 
-				System.out.println(arraylistname.get(i) + " CONSONANT");
+				System.out.println(arraylistFullName.get(i) + " CONSONANT");
 
 			}
-		}		
+		}
 
-		//Mapear letras nombre
-		
-		Map<Character,Integer> contadorletras = new HashMap<Character,Integer>();
-		
-		for (char l : arraylistname) {
-			
+		// Mapear letras nombre completo
+
+		Map<Character, Integer> contadorletras = new HashMap<Character, Integer>();
+
+		for (char l : arraylistFullName) {
+
 			char key = Character.toUpperCase(l);
-			
+
 			if (Character.isAlphabetic(key)) {
 
-				//	contadorletras.compute(key, contadorletras.get(key));
-				
 				if (contadorletras.get(key) == null) {
-					
+
 					contadorletras.put(key, 1);
-					
+
 				} else {
-					
+
 					contadorletras.put(key, contadorletras.get(key) + 1);
 
 				}
@@ -63,10 +95,9 @@ public class LletresRepetidesAPP {
 
 		}
 
-		//Mostrar en pantalla los datos
-		
-		System.out.println(contadorletras.entrySet()); 
-			
-		
+		// Mostrar en pantalla los datos
+
+		System.out.println(contadorletras.entrySet());		
+
 	}
 }
