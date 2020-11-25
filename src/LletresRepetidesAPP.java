895c4d43cc70a5d25badefdc37d00a10bb24caf9
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-26 00:06:44 +0100

    Fase 3 Completada

--- src/LletresRepetidesAPP.java
+++ src/LletresRepetidesAPP.java
@@ -1,44 +1,72 @@
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class LletresRepetidesAPP {
 
 	public static void main(String[] args) {
 
-	ArrayList <Character> nombre= new ArrayList ();
-	
-	nombre.add('G');
-	nombre.add('E');
-	nombre.add('R');
-	nombre.add('A');
-	nombre.add('R');
-	nombre.add('D');
-	nombre.add('1');
-	
-		for (int i=0;i<nombre.size();i++) {
-		System.out.println(nombre.get(i));
-			if (nombre.get(i)== 'A'||
-				nombre.get(i)== 'E'||
-				nombre.get(i)== 'I'||
-				nombre.get(i)== 'O'||
-				nombre.get(i)== 'U') {
-				System.out.println("VOCAL");
-				
-			}else if(nombre.get(i)== '0'||
-				nombre.get(i)== '1'||
-				nombre.get(i)== '2'||
-				nombre.get(i)== '3'||
-				nombre.get(i)== '4'||
-				nombre.get(i)== '5'||
-				nombre.get(i)== '6'||
-				nombre.get(i)== '7'||
-				nombre.get(i)== '8'||
-				nombre.get(i)== '9'){
-				System.out.println("Els noms de persones no contenen números!");
-			}else{
-				System.out.println("CONSONANT");
-			}
+		// Nom amb String
+
+		String name = "Gerard";
+
+		// Convertir el nombre en una Arraylist
+
+		ArrayList<Character> arraylistname = new ArrayList();
+
+		for (int i = 0; i < name.length(); i++) {
+			arraylistname.add(name.charAt(i));
 		}
+
+		// Imprime si son vocales o consonantes y detecta números
+
+		for (int i = 0; i < arraylistname.size(); i++) {
+
+			char letra = Character.toUpperCase(arraylistname.get(i));
+
+			if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
+
+				System.out.println(arraylistname.get(i) + " VOCAL");
+
+			} else if (Character.isDigit(letra)) {
+
+				System.out.println(arraylistname.get(i) + " Els noms de persones no contenen números!");
+
+			} else {
+
+				System.out.println(arraylistname.get(i) + " CONSONANT");
+
+			}
+		}		
+
+		//Mapear letras nombre
+		
+		Map<Character,Integer> contadorletras = new HashMap<Character,Integer>();
+		
+		for (char l : arraylistname) {
+			
+			char key = Character.toUpperCase(l);
+			
+			if (Character.isAlphabetic(key)) {
+
+				//	contadorletras.compute(key, contadorletras.get(key));
+				
+				if (contadorletras.get(key) == null) {
+					
+					contadorletras.put(key, 1);
+					
+				} else {
+					
+					contadorletras.put(key, contadorletras.get(key) + 1);
+
+				}
+			}
+
+		}
+
+		//Mostrar en pantalla los datos
+		
+		System.out.println(contadorletras.entrySet()); 
+			
 		
 	}
 }
-
