d480a29a72fff935bcb269298fb6f9bdf82794d8
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-26 17:46:17 +0100

    Corretgit error

--- src/LletresRepetidesAPP.java
+++ src/LletresRepetidesAPP.java
@@ -3,7 +3,6 @@ public class LletresRepetidesAPP {
 	public static void main(String[] args) {
 		
 		char[] nombre = { 'G', 'E', 'R', 'A', 'R', 'D' };
-		System.out.println(nombre[0]);
 
 		for (int i = 0; i <= 5; i++) {
 			System.out.println(nombre[i]);
