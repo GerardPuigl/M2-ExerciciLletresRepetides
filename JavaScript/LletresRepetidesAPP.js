bd81e9d439e72e5c5b5a10b0358ee4d7f5a6fc49
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-12-05 01:29:03 +0100

    Fase 3 Completa

--- JavaScript/LletresRepetidesAPP.js
+++ JavaScript/LletresRepetidesAPP.js
@@ -3,36 +3,43 @@
 var nombre = new Array;
 
 nombre.push('G');
-nombre.push('E');
-nombre.push('R');
-nombre.push('A');
-nombre.push('R');
-nombre.push('D');
+nombre.push('e');
+nombre.push('r');
+nombre.push('a');
+nombre.push('r');
+nombre.push('d');
 nombre.push('1');
 
-// Comprobar si són vocales, consonates y números
+// Comprobar si son vocales, consonates y números
 for (var i = 0; i < nombre.length; i++) {
     document.writeln(nombre[i], "<br/>");
-    if (nombre[i] == 'A' ||
-        nombre[i] == 'E' ||
-        nombre[i] == 'I' ||
-        nombre[i] == 'O' ||
-        nombre[i] == 'U') {
+    if (/[À-ú,a,e,i,o,u,A,E,I,O,U]/.test(nombre[i])) {
         document.writeln("VOCAL", "<br/>");
 
-    } else if (nombre[i] == '0' ||
-        nombre[i] == '1' ||
-        nombre[i] == '2' ||
-        nombre[i] == '3' ||
-        nombre[i] == '4' ||
-        nombre[i] == '5' ||
-        nombre[i] == '6' ||
-        nombre[i] == '7' ||
-        nombre[i] == '8' ||
-        nombre[i] == '9') {
+    } else if (/[1-9]/.test(nombre[i])) {
         document.writeln("Els noms de persones no contenen números!", "<br/>");
     } else {
         document.writeln("CONSONANT", "<br/>");
     }
 }
+
+//Mapear letras nombre
+		
+var contadorletras = new Map;
+
+for (let index = 0; index < nombre.length; index++) {
+
+    if (/[A-zÀ-ú]/.test(nombre[index])){
+
+        if (contadorletras.has(nombre[index].toUpperCase())){
+            contadorletras.set(nombre[index].toUpperCase(), contadorletras.get(nombre[index].toUpperCase())+1);
+        }else{
+            contadorletras.set(nombre[index].toUpperCase(), 1);
+        }
+
+    }
+
+}
+
+contadorletras.forEach((key,value)=>{document.write(key,value,"</br>")});
 
